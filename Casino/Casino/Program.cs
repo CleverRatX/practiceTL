64bf62890e72da15b559e5b0cfc4aab2249a1c2b
CleverRatX <193495079+CleverRatX@users.noreply.github.com>
2026-08-04 02:36:53 +0300

    Casino: исправление ошибок

--- Casino/Casino/Program.cs
+++ Casino/Casino/Program.cs
@@ -10,46 +10,35 @@
 public class Casino
 {
     private int _balance = 0;
-    private bool _isGameStarts = false;
+    private bool _isGameStarts = true;
     private const int Multiplicator = 3;
     private const int MinNum = 18;
 
     public void RunCasino()
     {
-        Console.Write( "Начать (да/нет): " );
-
-        string? answer = Console.ReadLine();
-        if ( answer != null )
+        bool askingForBalance = true;
+        while ( askingForBalance )
         {
-            answer = answer.ToLower();
-            if ( answer == "да" || answer == "yes" || answer == "y" )
+            Console.Write( "Введите начальный баланс: " );
+            string? input = Console.ReadLine();
+            if ( TryParseAndSetBalance( input ) )
             {
-                StartGame();
-
-                while ( true )
-                {
-                    Console.Write( "Введите начальный баланс: " );
-                    string? input = Console.ReadLine();
-                    if ( ParseAndSetBalance( input ) )
-                    {
-                        break;
-                    }
-                }
-
-                HandleTitle();
-                HandleMenu();
-
-                while ( _isGameStarts )
-                {
-                    Console.WriteLine();
-                    Console.Write( "Введите команду: " );
-                    ProcessInput();
-                }
+                askingForBalance = false;
             }
+        }
+
+        PrintTitle();
+        PrintMenu();
+
+        while ( _isGameStarts )
+        {
+            Console.WriteLine();
+            Console.Write( "Введите команду: " );
+            HandleInput();
         }
     }
 
-    private bool ParseAndSetBalance( string? inputBal )
+    private bool TryParseAndSetBalance( string? inputBal )
     {
         if ( int.TryParse( inputBal, out int bal ) && bal > 0 )
         {
@@ -63,7 +52,7 @@ public class Casino
         }
     }
 
-    private bool ParseAndAddToBalance( string? inputAdd )
+    private bool TryParseAndAddToBalance( string? inputAdd )
     {
         if ( int.TryParse( inputAdd, out int addition ) && addition > 0 )
         {
@@ -77,22 +66,17 @@ public class Casino
         }
     }
 
-    private void HandleBalance()
-    {
-        Console.WriteLine( $"Ваш баланс: {_balance}" );
-    }
-
-    private void StartGame()
-    {
-        _isGameStarts = true;
-    }
-
     private void EndGame()
     {
         _isGameStarts = false;
     }
 
-    private void HandleTitle()
+    private void PrintBalance()
+    {
+        Console.WriteLine( $"Ваш баланс: {_balance}" );
+    }
+
+    private void PrintTitle()
     {
         Console.WriteLine();
         Console.WriteLine( " ####    ##    ####  # #    #  #### " );
@@ -104,7 +88,7 @@ public class Casino
         Console.WriteLine();
     }
 
-    private void HandleMenu()
+    private void PrintMenu()
     {
         Console.WriteLine( "Доступные команды:" );
         Console.WriteLine( "deposit [сумма пополнения] - пополнить баланс" );
@@ -114,114 +98,114 @@ public class Casino
         Console.WriteLine( "exit - выход" );
     }
 
-    private void RunGame( int sum )
+    private void RunGameIteration( int betSum )
     {
-        Console.WriteLine( $"Игра началась, ваша ставка: {sum}" );
+        Console.WriteLine( $"Игра началась, ваша ставка: {betSum}" );
 
         int randomNum = Random.Shared.Next( 1, 21 );
         Console.WriteLine( $"Выпало число: {randomNum}" );
 
         if ( randomNum >= MinNum )
         {
-            int winSum = sum * ( 1 + ( Multiplicator * ( randomNum % ( MinNum - 1 ) ) ) );
+            int winSum = betSum * ( 1 + Multiplicator * ( randomNum % ( MinNum - 1 ) ) );
             Console.WriteLine( $"Вы выиграли!!! Сумма выигрыша: {winSum}" );
             _balance += winSum;
-            HandleBalance();
+            PrintBalance();
         }
         else
         {
             Console.WriteLine( "Вы проиграли..." );
-            _balance -= sum;
-            HandleBalance();
+            _balance -= betSum;
+            PrintBalance();
         }
     }
 
-    private void TryProcessBet( string? inputBetSum )
+    private void TryHandleBet( string? inputBetSum )
     {
-        if ( int.TryParse( inputBetSum, out int betSum ) )
-        {
-            if ( betSum <= _balance )
-            {
-                if ( betSum > 0 )
-                {
-                    RunGame( betSum );
-                }
-                else
-                {
-                    Console.WriteLine( "Ошибка: сумма ставки должна быть > 0" );
-                }
-            }
-            else
-            {
-                Console.WriteLine( "Ошибка: сумма ставки превышает баланс" );
-            }
-        }
-        else
+        if ( !int.TryParse( inputBetSum, out int betSum ) )
         {
             Console.WriteLine( "Ошибка: некорректная сумма ставки" );
+            return;
         }
+
+        if ( betSum > _balance )
+        {
+            Console.WriteLine( "Ошибка: сумма ставки превышает баланс" );
+            return;
+        }
+
+        if ( betSum <= 0 )
+        {
+            Console.WriteLine( "Ошибка: сумма ставки должна быть > 0" );
+            return;
+        }
+
+        RunGameIteration( betSum );
     }
 
-    private void ProcessInput()
+    private void HandleInput()
     {
         string? input = Console.ReadLine();
-        if ( input != null )
-        {
-            string[] parts = input.Split( ' ' );
-            if ( parts.Length == 1 )
-            {
-                switch ( parts[ 0 ] )
-                {
-                    case "bal":
-                        {
-                            HandleBalance();
-                        }
-                        break;
-                    case "menu":
-                        {
-                            HandleMenu();
-                        }
-                        break;
-                    case "exit":
-                        {
-                            EndGame();
-                        }
-                        break;
-                    default:
-                        Console.WriteLine( "Ошибка: некорректная команда" );
-                        break;
-                }
-            }
-            else if ( parts.Length > 1 )
-            {
-                switch ( parts[ 0 ] )
-                {
-                    case "deposit":
-                        {
-                            if ( ParseAndAddToBalance( parts[ 1 ] ) )
-                            {
-                                HandleBalance();
-                            }
-                        }
-                        break;
-                    case "bet":
-                        {
-                            TryProcessBet( parts[ 1 ] );
-                        }
-                        break;
-                    default:
-                        Console.WriteLine( "Ошибка: некорректная команда" );
-                        break;
-                }
-            }
-            else
-            {
-                Console.WriteLine( "Ошибка" );
-            }
-        }
-        else
+
+        if ( string.IsNullOrWhiteSpace( input ) )
         {
             Console.WriteLine( "Ошибка: пустой ввод" );
+            return;
+        }
+
+        string[] parts = input.Split( ' ' );
+
+        if ( parts.Length > 2 )
+        {
+            Console.WriteLine( "Ошибка: слишком много аргументов в вводе" );
+            return;
+        }
+
+        switch ( parts[ 0 ] )
+        {
+            case "bal":
+                {
+                    PrintBalance();
+                }
+                break;
+            case "menu":
+                {
+                    PrintMenu();
+                }
+                break;
+            case "exit":
+                {
+                    EndGame();
+                }
+                break;
+            case "deposit":
+                {
+                    if ( parts.Length < 2 )
+                    {
+                        Console.WriteLine( "Ошибка: отсутствует аргумент для команды \"deposit\"" );
+                        return;
+                    }
+
+                    if ( TryParseAndAddToBalance( parts[ 1 ] ) )
+                    {
+                        PrintBalance();
+                    }
+                }
+                break;
+            case "bet":
+                {
+                    if ( parts.Length < 2 )
+                    {
+                        Console.WriteLine( "Ошибка: отсутствует аргумент для команды \"bet\"" );
+                        return;
+                    }
+
+                    TryHandleBet( parts[ 1 ] );
+                }
+                break;
+            default:
+                Console.WriteLine( "Ошибка: некорректная команда" );
+                break;
         }
     }
 }
