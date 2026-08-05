300bd7f042102e3633ea60436236b61c3ab10dfe
CleverRatX <193495079+CleverRatX@users.noreply.github.com>
2026-08-05 20:25:25 +0300

    OrderManager - Краснопольский Ярослав (#1)
    * OrderManager: initial commit
    
    * OrderManager: исправление ошибок
    
    * OrderManager: Исправлен бесконечный цикл
    
    * OrderManager: Исправлено именование логики подтверждения заказа

---
+++ OrderManager/OrderManager/Program.cs
@@ -0,0 +1,73 @@
+﻿public class Program
+{
+    private static readonly HashSet<string> _agreeAnswers = [ "да", "yes", "y" ];
+
+    public static void Main( string[] args )
+    {
+        string product = RequestString( "Введите название товара: " );
+        int count = RequestInt( "Введите количество: " );
+        string name = RequestString( "Введите ваше имя: " );
+        string address = RequestString( "Введите адрес доставки: " );
+
+        Console.WriteLine( $"Здравствуйте, {name}, вы заказали {count} {product} на адрес {address}, все верно?" );
+        bool isAgree = AskForAgreement();
+
+        if ( isAgree )
+        {
+            DateTime deliveryDate = DateTime.Now.AddDays( 3 );
+            Console.WriteLine( $"{name}! Ваш заказ {product} в количестве {count} оформлен! Ожидайте доставку по адресу {address} к {deliveryDate:dd.MM.yyyy}" );
+        }
+        else
+        {
+            Console.WriteLine( "Заказ отменён" );
+        }
+    }
+
+    private static string RequestString( string message )
+    {
+        Console.Write( message );
+        string? input = Console.ReadLine();
+
+        while ( string.IsNullOrWhiteSpace( input ) )
+        {
+            Console.WriteLine( "Ошибка: Ввод не должен быть пустым" );
+            Console.Write( message );
+            input = Console.ReadLine();
+        }
+
+        return input;
+    }
+
+    private static int RequestInt( string message )
+    {
+        int result = 0;
+        bool isParsed = false;
+        while ( !isParsed )
+        {
+            Console.Write( message );
+            string? input = Console.ReadLine();
+
+            isParsed = int.TryParse( input, out result );
+            if ( !isParsed )
+            {
+                Console.WriteLine( "Ошибка: Вы ввели не число" );
+            }
+        }
+        return result;
+    }
+
+    private static bool AskForAgreement()
+    {
+        string? answer = Console.ReadLine();
+
+        if ( !string.IsNullOrWhiteSpace( answer ) )
+        {
+            answer = answer.ToLower();
+            if ( _agreeAnswers.Contains( answer ) )
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
